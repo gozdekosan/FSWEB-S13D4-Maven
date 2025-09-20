84fafa2f66f13fe5f3eff951e588a8368cbe357a
gozdekosan <gkosann@gmail.com>
2025-09-20 23:43:24 +0300

    S13D4 Projesi tamamlandı.

---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public enum Weapon {
+    SWORD(9,1.7);
+
+    private final int damage;
+    private final double attackSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+}
+
+
+
